8b25820763f53c7cad3d7eee05769721ad9d5d16
Khaled Janada <khaledjanada@mans.edu.eg>
2022-09-03 16:11:42 +0200

    ...

--- src/read_data.py
+++ src/read_data.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
 yr_from = 1933
-yr_to = 2023
+yr_to = 2004
 
 for n in range(yr_from, yr_to):
-    scoring_table = pd.read_html('https://www.hockey-reference.com/teams/DET/1933.html', match='PIM',
+    # This season was cancelled
+    if n == 2005:
+        continue
+    scoring_table = pd.read_html(f'https://www.hockey-reference.com/teams/DET/{n}.html', match='PIM',
                                  header=0, skiprows=1)[0]
     scoring_table['Player'] = scoring_table['Player'].str.replace('*', '', regex=False)
     scoring_table.drop(scoring_table.index[-1], inplace=True)
